e8475a81093666a8bb83b969358c44a288c13d4b
linnene <2691234891@qq.com>
2026-01-09 11:16:40 +0800

    feat:调整项目文件结构

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 
-from config import config
+from neuro_sama.config.config import config
 import os
 
 """
 test1:
 """
 
-from src.neuro_sama.parser import parse_jsonl_file, save_as_jsonl
-from src.neuro_sama.models.dialogue import BaseMes
+from neuro_sama.parser import parse_jsonl_file, save_as_jsonl
+from neuro_sama.models.dialogue import BaseMes
 
 output_path = config.cleaned_dir
 files = os.listdir(config.raw_dir)
@@ -21,9 +21,9 @@ test2:
 测试将 pending_data 目录下的所有文件，使用 build_repeat_segments 进行解析后，打印结果
 """
 
-from src.neuro_sama.parser import build_repeat_segments_Iterator, read_jsonl_file
+from neuro_sama.parser import build_repeat_segments_Iterator, read_jsonl_file
 
 files = os.listdir(config.cleaned_dir)
 
 for file in files:
-    build_repeat_segments_Iterator(read_jsonl_file(config.cleaned_dir+file))
+    print(build_repeat_segments_Iterator(read_jsonl_file(config.cleaned_dir+file)))
